2d7f2eb709d5283b933a62e212a35e7f0a7037a3
Temitope Oladele <temitope.oladele@gmail.com>
2022-09-02 10:27:11 -0500

    Update streamlit_app.py
    imported pandas and added fruit list

--- streamlit_app.py
+++ streamlit_app.py
@@ -7,6 +7,8 @@ streamlit.text ('🥣 Omega 3 & Bluberry Oatmeal')
 streamlit.text ('🥗 Kale, Spinach & Rocket Smoothie')
 streamlit.text ('🐔 Hard-Boiled Free Range Egg')
 streamlit.text ('🥑🍞 Avocado Toast')
-
-
 streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
+
+import pandas as pd
+my_fruit_list = pd.read('https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt')
+streamlit.daraframe(my_fruit_list)
