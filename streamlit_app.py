0b990598f460b6792f20cfb6cff6da1ba8187403
Temitope Oladele <temitope.oladele@gmail.com>
2022-09-02 10:22:28 -0500

    Update streamlit_app.py
    corrected emoji

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,7 +2,8 @@ import streamlit
 
 streamlit.title('My Parent New Healthy Dinner')
 
-streamlit.header('🥣 Breakfast Menu')
-streamlit.text ('🥗 Omega 3 & Bluberry Oatmeal')
-streamlit.text ('🐔 Kale, Spinach & Rocket Smoothie')
-streamlit.text ('🥑🍞 Hard-Boiled Free Range Egg')
+streamlit.header('Breakfast Menu')
+streamlit.text ('🥣 Omega 3 & Bluberry Oatmeal')
+streamlit.text ('🥗 Kale, Spinach & Rocket Smoothie')
+streamlit.text ('🐔 Hard-Boiled Free Range Egg')
+streamlit.text ('🥑🍞 Avocado Toast')
