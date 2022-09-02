214eecdca606ae3f843f4af0e973332eff9acb68
Temitope Oladele <temitope.oladele@gmail.com>
2022-09-02 10:16:40 -0500

    Update streamlit_app.py
    Added header and texts

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,3 +1,8 @@
 import streamlit 
 
 streamlit.title('My Parent New Healthy Dinner')
+
+streamlit.header('Breakfast Menu')
+streamlit.text ('Omega 3 & Bluberry Oatmeal')
+streamlit.text ('Kale, Spinach & Rocket Smoothie')
+streamlit.text ('Hard-Boiled Free Range Egg')
