915b93ce02ccdfc4e1900125f86a21ecae922b8b
Temitope Oladele <temitope.oladele@gmail.com>
2022-09-02 11:28:53 -0500

    Update streamlit_app.py
    Added ['Avocado', 'Strawberries'])

--- streamlit_app.py
+++ streamlit_app.py
@@ -15,7 +15,7 @@ my_fruit_list = pd.read_csv('https://uni-lab-files.s3.us-west-2.amazonaws.com/da
 my_fruit_list = my_fruit_list.set_index('Fruit')
 
 #Add an interactive widget called Multi-select
-streamlit.multiselect("Pick some fruits: ", list(my_fruit_list.index))
+streamlit.multiselect("Pick some fruits: ", list(my_fruit_list.index), ['Avocado', 'Strawberries'])
 
 #display the table on the page
 streamlit.dataframe(my_fruit_list)
